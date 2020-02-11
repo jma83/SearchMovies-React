bfbcaf3f7880f2756c7e92ecf20c35336447fcb9
javier.martinez <jma96.elx@gmail.com>
2020-02-11 23:39:18 +0100

    Start calling API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Title } from './components/Title'
 import { SearchForm } from './components/SearchForm'
 import './App.css';
 import 'bulma/css/bulma.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Search Movies</Title>
-      <div className="SearchForm-wrapper">
-        <SearchForm/>
+class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) =>{
+    if (results === undefined)
+    results = []
+    this.setState({results})
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="SearchForm-wrapper">
+          <SearchForm onResults={this._handleResults}/>
+        </div>
+        {this.state.results.length === 0 ? <p>Sin Resultados</p> : <p>Con Resultados</p>}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,41 @@
 import React,{Component} from 'react';
+//http://www.omdbapi.com/
+const API_KEY = 'c7629dea'
 
 export class SearchForm extends Component {
-    state = {  }
+    state = { 
+        inputMovie: ''
+     }
+
+     _handleChange = (e) => {
+        this.setState({ inputMovie: e.target.value })
+     }
+
+     _handleSubmit = (e) => {
+        e.preventDefault();
+        const{ inputMovie } = this.state;
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`).then(res => res.json())
+        .then(results => {const {Search, totalResults} = results
+            console.log({Search,totalResults})
+            this.props.onResults(Search)} );
+     }
+
     render() { 
-        return ( 
-            <div className="field has-addons">
-                <div className="control">
-                    <input class="input" type="text" placeholder="Find a repository"/>
+        return (
+            <form onSubmit={this._handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <input className="input" 
+                        onChange={this._handleChange}
+                        type="text" placeholder="Find a repository"/>
+                    </div>
+                    <div className="control">
+                        <button className="button is-info">
+                        Search
+                        </button>
+                    </div>
                 </div>
-                <div className="control">
-                    <button className="button is-info">
-                    Search
-                    </button>
-                </div>
-            </div>            
+            </form>          
          );
     }
 }
